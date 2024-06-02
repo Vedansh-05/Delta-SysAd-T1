01123eaa08691a049879d3f0750024a1552a1ce8
Vedansh-05 <110123121@nitt.edu>
2024-06-02 17:49:05 +0530

    Add the .bashrc file with aliases
    Added the .bashrc file with the respective aliases
    Added the logic for only core to run mentorallocation and displaystatus alias

---
+++ .bashrc
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias userGen='./setup.sh'
+
+alias domainPref='./domain-add.sh'
+
+alias mentorAllocation='./mentor-allocation.sh'
+
+alias submitTask='./submitTask.sh'
+
+alias displayStatus='./displayStatus.sh'
--- displayStatus.sh
+++ displayStatus.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
 
-
-if [ "$#" -gt 0 ] 
+if [ $USER = core ]
 then
-    domains=("$@")
-else
-    domains=( web app sysad )
-fi
+    if [ "$#" -gt 0 ] 
+    then
+        domains=("$@")
+    else
+        domains=( web app sysad )
+    fi
 
-touch "/home/core/submitted_list.txt"
-last_timestamp=$(sort -n -k 1 /home/core/submitted_list.txt | tail -1 | awk '{print $1}')
+    touch "/home/core/submitted_list.txt"
+    last_timestamp=$(sort -n -k 1 /home/core/submitted_list.txt | tail -1 | awk '{print $1}')
 
-for domain in "${domains[@]}"
-do
-    total_mentees=$(grep -c "$domain" "/home/core/mentees_domain.txt")
-    tasks=( task1 task2 task3 )
-
-    for task in "${tasks[@]}"
+    for domain in "${domains[@]}"
     do
-        task_completed=0
+        total_mentees=$(grep -c "$domain" "/home/core/mentees_domain.txt")
+        tasks=( task1 task2 task3 )
 
-        while IFS=' ', read -r name roll_number;do
-            if grep -q "${domain}-${task}" "/home/core/mentees/$name/task_completed.txt";
-            then
-                (( task_completed++ ))
+        for task in "${tasks[@]}"
+        do
+            task_completed=0
 
-                echo "$(date +%s) $name $roll_number $domain $task $(cat /home/core/mentees/$name/$domain/$task/submissions.txt)" >> "/home/core/submitted_list.txt"
+            while IFS=' ', read -r name roll_number;do
+                if grep -q "${domain}-${task}" "/home/core/mentees/$name/task_completed.txt";
+                then
+                    (( task_completed++ ))
 
-            fi
-        done < "$(awk '/$domain/ {print $2,$1}' /home/core/mentees_domain.txt)"
-        percentage_submitted=$((task_completed*100/total_mentees))
-        echo "${domain} ${task} percentage= ${percentage_submitted}%"
+                    echo "$(date +%s) $name $roll_number $domain $task $(cat /home/core/mentees/$name/$domain/$task/submissions.txt)" >> "/home/core/submitted_list.txt"
+
+                fi
+            done < "$(awk '/$domain/ {print $2,$1}' /home/core/mentees_domain.txt)"
+            percentage_submitted=$((task_completed*100/total_mentees))
+            echo "${domain} ${task} percentage= ${percentage_submitted}%"
+        done
+        echo
     done
-    echo
-done
-echo "New Submissions since last run:"
-awk -v timestamp="$last_timestamp" '$1 > $timestamp {print $2,$3,$4,$5,$6}' "/homecore/submitted_list.txt"
+    echo "New Submissions since last run:"
+    awk -v timestamp="$last_timestamp" '$1 > $timestamp {print $2,$3,$4,$5,$6}' "/homecore/submitted_list.txt"
+else 
+    echo "Only core can use this alias"
+fi
